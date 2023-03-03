5d1f6fee13ca19aa0ecb0c03905b28df92ec74a9
e.tairov <ev.tairov@yandex.ru>
2023-03-03 16:35:24 +0700

    09_1

--- Home_9_task_01/Program.cs
+++ Home_9_task_01/Program.cs
@@ -5,3 +5,40 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 //--------------------------------
+
+int GetNaturalNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или некорректное число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+void PrintNumber(int N, int i)
+{
+    if(i != 0)
+    {
+        Console.Write($"{i} ");
+        i--;
+        PrintNumber(N, i);
+    }
+}
+
+Console.WriteLine();
+int numbers = GetNaturalNumber("Введите натуральное число: ");
+int i = numbers;
+Console.Write($"N = {numbers} --> ");
+PrintNumber(numbers, i);
