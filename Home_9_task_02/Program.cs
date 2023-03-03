63cfd36739160360fc92e6f02f7e9dc6089a9729
e.tairov <ev.tairov@yandex.ru>
2023-03-04 02:01:15 +0700

    09-2

--- Home_9_task_02/Program.cs
+++ Home_9_task_02/Program.cs
@@ -5,3 +5,27 @@ M = 4; N = 8. -> 30
 */
 //-------------------------
 
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NumberRec(m, n));
+Console.WriteLine($"Сумма Элементов = {SumNumbers(m, n)}");
+Console.ReadLine();
+
+string NumberRec(int m, int n)
+{
+    if (m <= n) return $"{m} " + NumberRec(m + 1, n);
+    else return String.Empty;
+}
+
+static int SumNumbers(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;            
+    if (n == 0) return (m * (m + 1)) / 2;       
+    if (m == n) return m;                       
+    if (m < n) return n + SumNumbers(m, n - 1); 
+    else return n + SumNumbers(m, n + 1);            
+}
