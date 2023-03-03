6e918e960391400fe18bb1f3ac39119b7fd78ced
e.tairov <ev.tairov@yandex.ru>
2023-03-04 02:19:15 +0700

    09_3

--- Home_9_task_03/Program.cs
+++ Home_9_task_03/Program.cs
@@ -3,3 +3,44 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
 //-----------------------------
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+/*-------------------------------------------------------
+int Akk(int n, int m)
+{
+if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+ 
+Console.WriteLine(Akk(1, 2)); // 4
+Console.WriteLine(Akk(0, 0)); // 1
+Console.WriteLine(Akk(4, 5)); // Stack Overflow !
+ 
+Console.ReadKey();
+*/
+//------------------------------------------------
